ade243a0c88c777daf282dc8ee6e20b5256732b9
Carlyle Douglas <cdouglascorp@gmail.com>
2020-02-01 17:03:19 -0700

    Add JS functions for page changing

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ function closeWelcome() {
   welcomeVanish.style.display = "none";
 
 }
+
+var main = document.querySelector('main');
+var transactions = document.querySelector('.transactions');
+var main2 = document.querySelector('.main2');
+
+transactions.addEventListener('click', changePage);
+
+function changePage() {
+  main.style.display = "none";
+  transactions.classList.add("buttonSelect");
+  main2.classList.remove("main2");
+
+}
