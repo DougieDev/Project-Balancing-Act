6933f2ac962d638e2c91eacd8982f2d36d59bde8
Carlyle Douglas <cdouglascorp@gmail.com>
2020-02-03 00:25:20 -0700

    Add event listeners and functions to bring back original page

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function closeWelcome() {
 var main = document.querySelector('main');
 var transactions = document.querySelector('.transactions');
 var main2 = document.querySelector('.main2');
+// var dashboard = document.querySelector('.dashboard');
 
 transactions.addEventListener('click', changePage);
 
@@ -18,5 +19,17 @@ function changePage() {
   main.style.display = "none";
   transactions.classList.add("buttonSelect");
   main2.classList.remove("main2");
+  dashboard.classList.remove("buttonSelect");
+  main2.style.display = "block";
+  // transactions.classList.add("buttonSelect");
+}
+var dashboard = document.querySelector('.dashboard');
 
+dashboard.addEventListener('click', changePageMain);
+
+function changePageMain() {
+  main2.style.display = "none";
+  dashboard.classList.add("buttonSelect");
+  main.style.display = "block";
+  transactions.classList.remove("buttonSelect");
 }
